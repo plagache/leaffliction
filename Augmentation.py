33b7919aebdef63cba3199f518452db991933638
plgch <plagache@protonmail.com>
2024-11-25 22:42:43 +0100

    add pathlib to Augmentation;

--- Augmentation.py
+++ Augmentation.py
@@ -1,10 +1,17 @@
 import Augmentor
+from pathlib import Path
 
-# input_directory = "images"
-input_directory = "images/Apple_Black_rot"
+input_directory = "images"
+# input_directory = "images/Apple_Black_rot"
 output_directory = "augmented_directory"
 
-pipeline = Augmentor.Pipeline(input_directory)
+cwd = Path.cwd()
+output_path = Path(cwd, output_directory, input_directory)
+output_directory = output_path.__str__()
+
+Path(output_path).mkdir(parents=True, exist_ok=True)
+
+pipeline = Augmentor.Pipeline(source_directory=input_directory, output_directory=output_directory)
 
 # pipeline.flip_left_right(probability=1)
 # pipeline.flip_top_bottom(probability=1)
@@ -18,6 +25,6 @@ pipeline.random_distortion(probability=1, grid_width=2, grid_height=2, magnitude
 # pipeline.random_contrast( probability=1, min_factor=0.7, max_factor=1.3)
 # pipeline.random_color(probability=1, min_factor=0.7, max_factor=1.3)
 
-pipeline.sample(6)
+pipeline.sample(1)
 
 pipeline.process()
