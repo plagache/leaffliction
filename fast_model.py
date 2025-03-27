ef626b0a18854090806005e3738dcfad7cb103e6
plgch <plagache@protonmail.com>
2025-03-27 13:27:24 +0100

    lr_find not working properly;
    add information about batches and nbr of items being process;

--- fast_model.py
+++ fast_model.py
@@ -25,14 +25,20 @@ if __name__ == "__main__":
     ).dataloaders(path)
     # dls.show_batch(max_n=6)
 
+    total_items = len(dls.train_ds)
+    batch_size = dls.bs
+    total_batches = len(dls.train)
+    print(f"number of items: {total_items}, batch_size: {batch_size}, number of batches: {total_batches}")
+    # 40k images is not optimal for training
+
     learn = vision_learner(dls, resnet18, metrics=accuracy)
     # results = learn.validate()
     # print(f"Validation accuracy: {results[1]:.2f}")
 
-    optimal_learning_rate = learn.lr_find()
-    print(f"Optimal learning rate: {optimal_learning_rate}")
-    learn.fine_tune(2, base_lr=optimal_learning_rate)
-    # learn.fine_tune(3)
+    # optimal_learning_rate = learn.lr_find()
+    # print(f"Optimal learning rate: {optimal_learning_rate}")
+    # learn.fine_tune(2, base_lr=optimal_learning_rate)
+    learn.fine_tune(3, base_lr=0.002)
 
     results = learn.validate()
     print(f"Validation accuracy: {results[1]:.2f}")
